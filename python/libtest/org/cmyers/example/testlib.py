fb26274806d08daec614afe53348705e6e44ad8d
Carl Myers <cmyers@pernixdata.com>
2016-02-24 15:51:45 -0800

    Adding python example test package

---
+++ python/libtest/org/cmyers/example/testlib.py
@@ -0,0 +1,18 @@
+import sys
+import unittest2
+
+from org.cmyers.example import lib
+
+class TestLib(unittest2.TestCase):
+    def setUp(self):
+        self.widget = lib.Lib()
+        self._expected_developers = [
+                'amling',
+                'cmyers',
+        ]
+    def test_developers(self):
+        self.assertEqual(self._expected_developers, self.widget.developers())
+
+if __name__ == '__main__':
+    unittest2.main()
+
